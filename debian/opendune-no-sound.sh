2446b5b5ff353dc5ae1fcaf9921c6e92deeb654e
Radek Polak <psonek2@seznam.cz>
2012-12-10 04:23:54 +0100

    qtmoko debian package

--- debian/opendune-no-sound.sh
+++ debian/opendune-no-sound.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 qcop service send RotationManager "setCurrentRotation(int)" 90
 export SDL_VIDEODRIVER=QtMoko
-export SDL_AUDIODRIVER=pulse
+export SDL_AUDIODRIVER=dummy
 #export SDL_QT_DEBUG=1
 export SDL_QT_FS=1
 #export SDL_QT_SCRIPT=/opt/qtmoko/share/sdl/opendune.js
