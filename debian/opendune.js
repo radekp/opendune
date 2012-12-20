5967135b3204c8897a49f7d36fc259b8d909a0ce
Radek Polak <psonek2@seznam.cz>
2012-12-20 17:29:39 +0100

    debian - key for leaving fullscreen, dont install no sound desktop

---
+++ debian/opendune.js
@@ -0,0 +1,6 @@
+(function(x, y, w, h)
+{
+    // Fullscreen toggle is in top left corner
+    if(x < w / 8 && y < h / 8) { sym = -1; return true; }
+}
+)
--- debian/opendune.sh
+++ debian/opendune.sh
@@ -4,7 +4,7 @@ export SDL_VIDEODRIVER=QtMoko
 export SDL_AUDIODRIVER=pulse
 #export SDL_QT_DEBUG=1
 export SDL_QT_FS=1
-#export SDL_QT_SCRIPT=/opt/qtmoko/share/sdl/opendune.js
+export SDL_QT_SCRIPT=/opt/qtmoko/share/sdl/opendune.js
 cd /opt/qtmoko/share/opendune
 opendune
 qcop service send RotationManager "setCurrentRotation(int)" 0
